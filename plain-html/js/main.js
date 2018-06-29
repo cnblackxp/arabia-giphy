08ff62980aa85d6bb1e159f875201e59233d6418
cnblack <hazem.marwan.huzayen@gmail.com>
2018-06-29 17:10:26 +0300

    update

--- plain-html/js/main.js
+++ plain-html/js/main.js
@@ -9,6 +9,16 @@ sidebarListArray.forEach(el => {
   })(el) );
 })
 
+const removeFullImageView = () => {
+  fullImageView.classList.add('hidden');
+};
+
+const exitFullImageView = document.querySelector('.exit-btn');
+const fullImageView = document.querySelector('.full-img-view')
+exitFullImageView.addEventListener('click', removeFullImageView);
+fullImageView.addEventListener('click', removeFullImageView);
+
+
 
 let currentDataSearch;
 
@@ -29,6 +39,14 @@ const load = (options) =>
     xhr.send();
   });
 
+
+const viewImage = (url, description) => {
+  document.querySelector('.full-img').src = "assets/noimage.png";
+  document.querySelector('.full-img-view').classList.remove('hidden');
+  setTimeout(() => document.querySelector('.full-img').src = url, 10);
+  document.querySelector('.full-img-view > .img-container > p').innerText = description;
+}
+
 const renderImages = (searchTerm, element) => {
   if (searchTerm !== currentDataSearch)
   {
@@ -43,6 +61,7 @@ const renderImages = (searchTerm, element) => {
       .then(result => JSON.parse(result))
       .then(json => json.data)
       .then(imgs => {
+        console.log(imgs);
         let html = "";
         let els = 0;
         imgs.forEach((el, i) => {
@@ -52,7 +71,7 @@ const renderImages = (searchTerm, element) => {
             html += '<div class="column">';
           els++;
           const img = el.images.preview_webp.url;
-          html += `<img src="${img}" style="width:100%">`;
+          html += `<img src="${img}" class="img" onclick="viewImage('${el.images.original.url}', '${el.id}')" style="width:100%">`;
           if (els === 6) {
             html += '</div>';
             els = 0;
